70d5fe4eefaeaa5378feea47a862088628621eef
Tuğçe Önder <nymph_tuuche@hotmail.com>
2012-04-03 21:42:37 +0300

    Update filter2D.c

--- filter2D.c
+++ filter2D.c
@@ -1,116 +1,117 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 #include<time.h>
+#include<math.h>
+#include<conio.h>
 
-long double **f(int N,int M,double sigma)
-{
-      long double **p;
-      double y,x,top=0.0;
-      int i,j;
-       //allocate memory for p
-       p=(long double **)malloc(N*sizeof(long double*));
-         for(i=0;i<M;i++){
-                          p[i]=(long double *)malloc(M*sizeof(long double));}
-       //find initial x and y values in equation                
-       x= -(double)(M-1)/2; 
-       y= -(double)(N-1)/2;
-       //exponential
-       for(i=0;i<N;i++){
-                        for(j=0;j<M;j++){
-           p[i][j]=exp(-((x*x)+(y*y))/(2*sigma*sigma));
-           x++;}
-           y++;
-           x= -(double)(M-1)/2;
-           //top+=p[i][j]; //sum of the values
-           }
-           
-           //for (i=0;i<N;i++){
-              //for(j=0;j<M;j++){
-                 //p[i][j]/=top;}} //divide elements of the array 
-                                     //  by sum of the values 
-           return p;
+double **e(double **ptr,int row,int column,float sigma){
+      int i,j,x=-(row-1)/2,y=-(column-1)/2;
+      double sum=0;
+      for(i=0;i<row;i++)
+         for(j=0;j<column;j++){
+           ptr[i][j]=exp(-((x+i)*(x+i)+(y+j)*(y+j))/(2*sigma*sigma));
+           sum+=ptr[i][j];
 }
-          
-int main()
-{
-    int bsatir,bsutun,i=0,j=0,k=0,a;
-    int asatir,asutun,x,y,m;
-    double sum=0.0,sigma;
-    long double **B,**C;
-    int **A;
-    //column and line for A
-    printf("A line:");
-    scanf("%d",&asatir);
-    printf("A column:");
-    scanf("%d",&asutun);
-    //allocate memory for A
-     A=(int **)malloc(asatir*sizeof(int*));
-         for(x=0;x<asatir;x++){
-                          A[x]=(int *)malloc(asutun*sizeof(int));}
-    
-    srand(time(NULL));
-    //create random numbers in A
-    for(a=0;a<asatir;a++){
-                          for(i=0;i<asatir;i++){
-        A[a][i]=rand()%10;
-        printf("%d ",A[a][i]);
-        } printf("\n");
-    }
-    //column and line for B
-    printf("B line:");
-    scanf("%d",&bsatir);
-    printf("B column:");
-    scanf("%d",&bsutun);
-    printf("enter sigma");
-    scanf("%lf",&sigma);
-    //take the elements of B from the function f
-    B=f(bsatir,bsutun,sigma);
-    //print B
-    for(i=0;i<bsatir;i++){
-                          for(a=0;a<bsutun;a++){
-    printf("%lf ",B[i][a]);}
-    printf("\n");}
-    
-    //allocate memory for C(output)
-    C=(long double **)malloc(bsatir*sizeof(long double*));
-      for(i=0;i<bsutun;i++){
-          C[i]=(long double *)malloc(bsutun*sizeof(long double));}
-    
-    
-    //create array C
-    for(x=0;x<bsatir;x++){
-       for(y=0;y<bsutun;y++){
-           for(k=0;k<asatir;k++){
-              for(m=0;m<asutun;m++){
-                 sum+=(long double)A[i][j]*B[k][m];
-                 i++; j++;}}
-                 C[x][y]=sum;
-                 i=i-(k-1);
-                 j=j-(m-1);
-                 sum=0.0;}}
-        
-        
+      for(i=0;i<row;i++)
+        for(j=0;j<column;j++)
+           ptr[i][j]/=sum;
 
-//print C
-        for(a=0;a<bsatir;a++){
-                              for(i=0;i<bsutun;i++){
-        printf("C[%d][%d]=%lf ",a,i,C[a][i]);
-        }
-        printf("\n");
+      return ptr;
 }
-//clean memory
-   for(i=0;i<asatir;i++){
-         free(A[i]); }
-         free(A);
-   for(k=0;i<bsatir;k++){
-         free(B[k]); }
-         free(B);
-   for(x=0;x<bsatir;x++){
-       free(C[x]); }
-       free(C);
-    
-    system("pause");
-    return 0;
+double **allocate(double **ptr,int row,int column){
+   int i;
+   ptr=(double **)calloc(row,sizeof(double));
+     for(i=0;i<row;i++)
+     
+   ptr[i]=(double *)calloc(column,sizeof(double));
+    return ptr;
 }
+void print(double **ptr,int row,int column){
+     int i,j;
+     for(i=0;i<row;i++){
+        for(j=0;j<column;j++)
+     printf("%f ",ptr[i][j]);
+     printf("\n");
+}}
 
+int main(){
+    double **arr2,**filter,**arr1,**Ix,**Iy,**Ixx,**Ixy,**Iyy;
+    double kernel[1][3]={1.0,-2.0,1.0};
+    float sigma;
+    int r1,r2,r3,c1,c2,c3,i,j,k,l;
+srand(time(NULL));
+
+    printf("enter row and column for array1\n");
+    scanf("%d%d",&r1,&c1);
+    printf("enter row and column for exp\n");
+    scanf("%d%d",&r2,&c2);
+    printf("enter sigma for exp\n");
+    scanf("%f",&sigma);
+    
+r3=(r1-r2)+1;
+c3=(c1-c2)+1;
+printf("\narr1\n");
+
+arr1=allocate(arr1,r1,c1);
+if(arr1){
+   for(i=0;i<r1;i++)
+      for(j=0;j<c1;j++)
+   arr1[i][j]=rand()%10;
+   print(arr1,r1,c1);
+
+}
+   printf("\narr2\n");
+   
+   arr2=allocate(arr2,r2,c2);
+   if(arr2){
+   arr2=e(arr2,r2,c2,sigma);
+   print(arr2,r2,c2);
+}
+  filter=allocate(filter,r3,c3);
+  
+  printf("\nfilter\n");
+  if(filter){
+for(i=0;i<r3;i++)
+   for(j=0;j<c3;j++)
+      for(k=0;k<r2;k++)
+         for(l=0;l<c2;l++)
+              filter[i][j]+=arr1[i+k][j+l]*arr2[k][l];
+              print(filter,r3,c3);
+}
+//Ix Ixx icin
+Ix=allocate(Ix,r3,c3-2);
+Ixx=allocate(Ixx,r3,c3-2);
+
+   for(i=0;i<r3;i++){
+      for(j=0; j<c3-2 ;j++){
+          Ix[i][j]=filter[i][j]*kernel[0][0]+filter[i][j+1]*kernel[0][1]
++filter[i][j+2]*kernel[0][2];
+      Ixx[i][j]=Ix[i][j]*Ix[i][j];
+}}
+       printf("\nIx\n"); print(Ix,r3,c3-2);
+       printf("\nIxx\n"); print(Ixx,r3,c3-2);
+       
+//Iy Iyy icin
+Iy=allocate(Iy,r3-2,c3);
+Iyy=allocate(Iyy,r3-2,c3);
+
+    for(j=0;j<c3;j++){
+       for(i=0;i<r3-2;i++){
+           Iy[i][j]=filter[i][j]*kernel[0][0]+filter[i+1][j]*kernel[0][1]
++filter[i+2][j]*kernel[0][2];
+           Iyy[i][j]=Iy[i][j]*Iy[i][j];
+}}
+       printf("\nIy\n"); print(Ix,r3-2,c3);
+       printf("\nIyy\n"); print(Ixx,r3-2,c3);
+       
+//Ixy icin
+Ixy=allocate(Ixy,r3-2,c3-2);
+for(i=0;i<r3-2;i++)
+      for(j=0; j<c3-2 ;j++)
+          Ixy[i][j]=Ix[i][j]*Iy[i][j];
+
+       printf("\nIxy\n"); print(Ixy,r3-2,c3-2);
+
+getch();
+return 0;
+}
